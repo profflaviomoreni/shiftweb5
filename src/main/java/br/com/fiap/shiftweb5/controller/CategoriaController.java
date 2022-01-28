c02dafc872809e77e31504b10f9c7038ae7d242e
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-01-28 20:33:51 -0300

    adicionando busca com native query

--- src/main/java/br/com/fiap/shiftweb5/controller/CategoriaController.java
+++ src/main/java/br/com/fiap/shiftweb5/controller/CategoriaController.java
@@ -34,8 +34,8 @@ public class CategoriaController {
 	
 	@CrossOrigin
 	@GetMapping()
-	public ResponseEntity<List<CategoriaModel>> findAll(){
-		return ResponseEntity.ok( categoriaRepository.findByAtivo(true) );
+	public ResponseEntity<Long> findAll(){
+		return ResponseEntity.ok( categoriaRepository.getTotalCategorias() );
 	}
 	
 }
