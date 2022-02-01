dde68804ad30aa88cd4adc0721e75a483cc2b819
profflaviomoreni <profflavio.moreni@fiap.com.br>
2022-01-31 22:10:14 -0300

    finalizando o front-end

--- src/main/java/br/com/fiap/shiftweb5/controller/UsuarioController.java
+++ src/main/java/br/com/fiap/shiftweb5/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import br.com.fiap.shiftweb5.repository.UsuarioRepository;
 @RequestMapping("/usuario")
 //http://localhost:8080/usuario
 //Request Method
+@CrossOrigin("*")
 public class UsuarioController {
 
 
@@ -86,6 +88,7 @@ public class UsuarioController {
 	
 	@SuppressWarnings("rawtypes")
 	@PostMapping
+	@CrossOrigin("*")
 	public ResponseEntity post(@RequestBody UsuarioModel usuarioModel ) {
 		System.out.println(usuarioModel);
 		
